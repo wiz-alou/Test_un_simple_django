35a1a5f9420fb6fe83a188a90fde026721907383
alou <alassanesall765@gmail.com>
2022-06-22 00:09:10 +0000

    apres circleci

--- Master_GL_INSTA/settings.py
+++ Master_GL_INSTA/settings.py
@@ -105,7 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
