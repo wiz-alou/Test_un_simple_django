1eb20808512b4bd0f783cb7ebccbe763adac0263
alou <alassanesall765@gmail.com>
2022-06-22 15:33:28 +0000

    apres herok

--- Master_GL_INSTA/settings.py
+++ Master_GL_INSTA/settings.py
@@ -80,16 +80,17 @@ WSGI_APPLICATION = 'Master_GL_INSTA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config()
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 
 # Password validation
