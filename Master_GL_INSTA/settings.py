f09f3ae572eec5a2132cc4b6249af5053ed5a380
alou <alassanesall765@gmail.com>
2022-06-22 01:38:18 +0000

    apres herok

--- Master_GL_INSTA/settings.py
+++ Master_GL_INSTA/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from django.contrib import messages
+import django_heroku
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uibr3*v0ic_r&r$h4x3=9)7x-k=uac9!1239s20o*7_c%yj%9j'
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://authentification-master-gl-ut.herokuapp.com/']
 
 
 # Application definition
